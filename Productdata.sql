31191053da59b6fada260eb3184140f29b8cafbf
ButterAmul <86927577+ButterAmul@users.noreply.github.com>
2021-07-05 10:28:16 +0530

    Category_added.sql

--- Productdata.sql
+++ Productdata.sql
@@ -4,3 +4,5 @@ Select *
 from Production.Product P
 Join Production.productSubcategory PS--Subcategory joined
 on P.Subcategoryid-Ps.SubcategoryID
+JOin Production.ProductCategory PC
+On Pc.CategoryId=Ps.SubcategoryId
